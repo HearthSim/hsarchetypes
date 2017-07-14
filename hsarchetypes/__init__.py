02e10be55747cf60180ec0ad263e702f8111c46f
Jerome Leclanche <jerome@leclan.ch>
2017-07-15 00:46:17 +0300

    Finish cross cluster prevalence implementation

--- hsarchetypes/__init__.py
+++ hsarchetypes/__init__.py
@@ -1,31 +1,51 @@
-def get_signature_components(matching_decks, observation_counts, thresholds):
-	card_prevalence_counts = {}
+def get_signature_components(matching_decks, observation_counts, thresholds, other_sigs):
+	prevalence_counts = {}
 	deck_occurrences = 0
-	ret = []
 
 	for digest, cards in matching_decks.items():
 		obs_count = observation_counts[digest]
 		deck_occurrences += obs_count
 		for include in cards:
 			key = (include["card_id"], include["dbf_id"])
-			if key not in card_prevalence_counts:
-				card_prevalence_counts[key] = 0
-			card_prevalence_counts[key] += obs_count
+			if key not in prevalence_counts:
+				prevalence_counts[key] = 0
+			prevalence_counts[key] += obs_count
 
 	if not deck_occurrences:
 		# Could not find any matching deck, break early
-		return ret
+		return []
 
-	for (card_id, dbf_id), observation_count in card_prevalence_counts.items():
+	return calculate_prevalences(prevalence_counts, deck_occurrences, thresholds, other_sigs)
+
+
+def calculate_prevalences(prevalence_counts, deck_occurrences, thresholds, other_sigs):
+	ret = []
+
+	for (card_id, dbf_id), observation_count in prevalence_counts.items():
 		prevalence = float(observation_count) / float(deck_occurrences)
+
+		count_in_other_archetypes = sum(
+			other_sigs.values(),
+			key=lambda x: int(dbf_id in x)
+		)
+		cluster_freq_modifier = _calc_cross_cluster_modifier(count_in_other_archetypes, len(other_sigs))
 
 		for threshold in sorted(thresholds.keys(), reverse=True):
 			if prevalence >= threshold:
-				weight = float(thresholds[threshold]) * prevalence
+				weight = float(threshold) * prevalence * cluster_freq_modifier
 				ret.append(({"card_id": card_id, "dbf_id": dbf_id}, weight))
 				break
 
 	return ret
+
+
+def _calc_cross_cluster_modifier(count_in_other_archetypes, num_other_archetypes):
+	if num_other_archetypes == 0:
+		return 1
+
+	p = (1 - count_in_other_archetypes / num_other_archetypes)
+	cluster_freq_modifier = p * p
+	return cluster_freq_modifier
 
 
 def classify_deck(deck, archetype_ids, signature_weights, distance_cutoff):
@@ -35,11 +55,32 @@ def classify_deck(deck, archetype_ids, signature_weights, distance_cutoff):
 		if archetype_id in signature_weights:
 			for dbf_id, weight in signature_weights[archetype_id].items():
 				if dbf_id in deck:
-					distance += weight * float(deck.count(dbf_id))
+					distance += weight
+					# * float(deck.count(dbf_id)) if we want to account for copies
+					# but we must also normalize for that somehow
+			distance /= sum(signature_weights[archetype_id].values())
 
-		if distance and distance >= distance_cutoff:
+
+		if distance and distance >= 0.25: #distance_cutoff:
 			distances.append((archetype_id, distance))
 
 	if distances:
 		distances = sorted(distances, key=lambda t: t[1], reverse=True)
 		return distances[0][0]
+
+
+# ToDo:
+	# Regenerate all signatures for a class at the same time
+	# Figure out distance cutoff?
+	# Write a reprocess command, e.g. water rogue and elemental rogue
+	# Add component category names to SignatureComponent
+	# Write a big ass test suite
+	# Add instrumentation around match strengths, frequency, distance to second best match
+	# Dealing with multiple close matches (e.g. 3 signature matches within 1% of each other)
+		# Throw it all out
+		# Randomly pick one
+		# Let the highest value win.
+		# Select the most popular archetype
+		# Use the one with strongest single card match
+		# Look at other features, e.g. game duration
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,104 @@
-def test_foo():
-	pass
+import pytest
+from hsarchetypes import get_signature_components, _calc_cross_cluster_modifier
+
+
+def test_calc_cross_cluster_modifier():
+
+	assert _calc_cross_cluster_modifier(0, 0) == 1
+
+	assert _calc_cross_cluster_modifier(0, 1) == 1
+	assert _calc_cross_cluster_modifier(1, 1) == 0
+
+	assert _calc_cross_cluster_modifier(0, 2) == 1
+	assert _calc_cross_cluster_modifier(1, 2) == .25
+	assert _calc_cross_cluster_modifier(2, 2) == 0
+
+	assert _calc_cross_cluster_modifier(0, 3) == 1
+	assert _calc_cross_cluster_modifier(1, 3) == pytest.approx(4 / 9.0)
+	assert _calc_cross_cluster_modifier(2, 3) == pytest.approx(1 / 9.0)
+	assert _calc_cross_cluster_modifier(3, 3) == 0
+
+
+
+def test_calculate_prevalences_for_one_archetype():
+	# deck_digest -> { card_id, dbf_id, count }
+	matching_decks = {}
+
+	# deck_digest -> total_games
+	observation_counts = {}
+
+	thresholds = {
+		.8: 1,
+		.3: 0.5,
+	}
+
+	# archetype_id -> {dbf_id -> weight }
+	other_sigs = {
+
+	}
+
+	components = get_signature_components(matching_decks, observation_counts, thresholds, other_sigs)
+
+	# Assert the final weight is the prevelance * threshold
+
+
+def test_calculate_prevalences_for_two_distinct_archetypes():
+	# deck_digest -> { card_id, dbf_id, count }
+	matching_decks = {}
+
+	# deck_digest -> total_games
+	observation_counts = {}
+
+	thresholds = {
+		.8: 1,
+		.3: 0.5,
+	}
+
+	# archetype_id -> {dbf_id -> weight }
+	other_sigs = {
+
+	}
+
+	components = get_signature_components(matching_decks, observation_counts, thresholds, other_sigs)
+
+
+	# Assert the final weight is the prevelance * threshold
+
+
+def test_calculate_prevalences_for_two_overlapping_archetypes():
+	# deck_digest -> { card_id, dbf_id, count }
+	matching_decks = {}
+
+	# deck_digest -> total_games
+	observation_counts = {}
+
+	thresholds = {
+		.8: 1,
+		.3: 0.5,
+	}
+
+	# archetype_id -> {dbf_id -> weight }
+	other_sigs = {
+
+	}
+
+	components = get_signature_components(matching_decks, observation_counts, thresholds, other_sigs)
+
+def test_calculate_prevalences_for_three_archetypes():
+	# deck_digest -> { card_id, dbf_id, count }
+	matching_decks = {}
+
+	# deck_digest -> total_games
+	observation_counts = {}
+
+	thresholds = {
+		.8: 1,
+		.3: 0.5,
+	}
+
+	# archetype_id -> {dbf_id -> weight }
+	other_sigs = {
+
+	}
+
+	components = get_signature_components(matching_decks, observation_counts, thresholds, other_sigs)
