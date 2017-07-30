9ac7352f453ada824cfd5a9183b03407c64364e2
Andrew Wilson <andrewmoorewilson@gmail.com>
2017-07-30 15:49:28 -0400

    Refactor archetype signatures to use py.test parameterization

--- tests/test_main.py
+++ tests/test_main.py
@@ -24,42 +24,49 @@ def test_calc_cross_cluster_modifier():
 	assert _calc_cross_cluster_modifier(3, 3) == 0
 
 
-def test_signature_components(dbf_db):
+@pytest.mark.parametrize(
+	'game_format',
+	["FT_STANDARD", ] # "FT_WILD"]
+)
+@pytest.mark.parametrize(
+	'player_class_name',
+	['DRUID', 'HUNTER', 'MAGE', 'PALADIN', 'PRIEST', 'ROGUE', 'SHAMAN', 'WARLOCK', 'WARRIOR']
+)
+def test_signature_components(dbf_db, game_format, player_class_name):
 	for snapshot in os.listdir(FIXTURE_SUITE):
+		print("Fixture: %s" % snapshot)
+
 		snapshot_path = os.path.join(FIXTURE_SUITE, snapshot)
 		with open(snapshot_path, "r") as f:
 			data = json.load(f)
 
 		archetype_map = data["archetype_map"]
+		player_class = CardClass[player_class_name]
+		class_data = data[game_format][player_class.name]
+		training_data = class_data["training"]
+		validation_data = class_data["validation"]
+		new_weights = calculate_signature_weights(training_data)
 
-		for game_format in ("FT_STANDARD", "FT_WILD"):
-			for player_class in CardClass:
-				if CardClass.DRUID <= player_class <= CardClass.WARRIOR:
-					class_data = data[game_format][player_class.name]
-					training_data = class_data["training"]
-					validation_data = class_data["validation"]
-					new_weights = calculate_signature_weights(training_data)
+		for expected_id, validation_decks in validation_data.items():
+			for digest, validation_deck in validation_decks.items():
+				deck = validation_deck["cards"]
+				assigned_id = classify_deck(
+					deck,
+					new_weights.keys(),
+					new_weights
+				)
+				template = "Assigned Archetype %s (%s). Expected: %s (%s)"
+				template += "\nDeck %s\n%s"
+				msg = template % (
+					assigned_id,
+					archetype_map.get(assigned_id, ""),
+					expected_id,
+					archetype_map.get(expected_id, ""),
+					digest,
+					to_pretty_deck(dbf_db, deck)
+				)
 
-					for expected_id, validation_decks in validation_data.items():
-						for digest, validation_deck in validation_decks.items():
-							deck = validation_deck["cards"]
-							assigned_id = classify_deck(
-								deck,
-								new_weights.keys(),
-								new_weights
-							)
-							template = "Assigned Archetype %s (%s). Expected: %s (%s)"
-							template += "\nDeck %s %s"
-							msg = template % (
-								assigned_id,
-								archetype_map.get(assigned_id, ""),
-								expected_id,
-								archetype_map.get(expected_id, ""),
-								digest,
-								to_pretty_deck(dbf_db, deck)
-							)
-
-							assert assigned_id == expected_id, msg
+				assert assigned_id == expected_id, msg
 
 
 def to_pretty_deck(dbf_db, deck):
@@ -67,4 +74,4 @@ def to_pretty_deck(dbf_db, deck):
 	for dbf_id, count in deck.items():
 		for i in range(count):
 			result.append(dbf_db[int(dbf_id)].name)
-	return result
+	return "\n".join(sorted(result))
